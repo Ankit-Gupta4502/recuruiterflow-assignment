c556d423cce9c20a137bd07f047a00bb7b394614
Ankit Gupta <ankit.gupta@kapturecrm.com>
2025-12-17 19:11:42 +0530

    Add destination field to AddBoxForm and set default weight value
    - Added a new field for destination country in the form schema.
    - Set default value for weight input to 0.
    - Updated input properties to ensure proper registration with the form handling.

--- app/components/shared-components/AddBoxForm.tsx
+++ app/components/shared-components/AddBoxForm.tsx
@@ -4,11 +4,13 @@ import { z } from 'zod'
 import { useForm, type SubmitHandler } from "react-hook-form"
 import { zodResolver } from '@hookform/resolvers/zod'
 import ShippingOrderServices from '~/services/ShippingOrderServices'
+import { classNameMerger } from '~/utils'
 
 const schema = z.object({
     name: z.string().trim().min(1, "Receiver's Name is required"),
-    weight: z.coerce.number().gt(0, "Number should be bigger than 0"),
-    color: z.string().trim().min(1, "Receiver's Name is required").default("#F2F2F2")
+    weight: z.coerce.number().gt(0, "Number should be bigger than 0").default(0),
+    color: z.string().trim().min(1, "Receiver's Name is required").default("#F2F2F2"),
+    destination: z.string().trim().min(1, "Destination country is required")
 })
 
 type formSchema = z.infer<typeof schema>
@@ -36,6 +38,7 @@ const AddBoxForm = () => {
                 <Input isError={!!errors.weight} errorMessage={errors?.weight?.message} inputProps={{
                     placeholder: "Enter weight in KG",
                     type: "number",
+                    defaultValue: 0,
                     ...register("weight")
                 }} label="Weight" />
 
@@ -43,15 +46,19 @@ const AddBoxForm = () => {
                 <Input inputProps={{
                     placeholder: "Enter weight in KG",
                     type: "color",
-                    className: "max-w-[100px] ",
+                    defaultValue:"#F2F2F2",
+                    className: "max-w-[50px]  py-0 px-0 border-0 [block-size:32px]",
                     ...register("color")
-                }} label="Box Color" />
+                }} label="Choose Box Color" />
 
                 <div>
                     <label htmlFor="" className='block mb-2 text-gray-700  font-medium ' >Destination Country</label>
-                    <select name="" id="" className='border border-gray-300 px-2 rounded-sm py-2 w-full' >
+                    <select {...register("destination")} name="" id="" className={classNameMerger('border  px-2 rounded-sm py-2 w-full', errors.destination ? "border-red-600 focus:outline-red-600" : "border-gray-300")} >
                         <option value="">Select Country</option>
                     </select>
+                    {errors.destination && <span className=" text-red-600 text-sm placeholder:text-red-600  " >
+                        {errors.destination?.message}
+                    </span>}
                 </div>
 
 
