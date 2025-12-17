fad7bc07983ef5985b75ea136565ce8f11807349
Ankit Gupta <ankit.gupta@kapturecrm.com>
2025-12-17 19:00:50 +0530

    Implement shipping orders fetching in Orders component
    - Added useEffect to fetch shipping orders using ShippingOrderServices.
    - Integrated error handling and logging for the fetch operation.
    - Refactored the component structure for better readability.

--- app/routes/orders.tsx
+++ app/routes/orders.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import ShippingOrderServices from '~/services/ShippingOrderServices'
 
 const Orders = () => {
+  const { current: { getShippingOrders } } = useRef(ShippingOrderServices)
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getShippingOrders()
+      if (error) {
+        console.error(error, "error in getting orders");
+      } else {
+        console.log(data, "dataaaa")
+      }
+    })()
+  }, [])
   return (
-<div className="max-w-6xl mx-auto mt-6 bg-white border border-gray-200 rounded-lg shadow-sm overflow-hidden">
-  <table className="w-full table-fixed text-sm">
-    <thead className="bg-gray-50 border-b border-b-gray-200">
-      <tr>
-        <th className="px-4 py-3 text-left font-medium text-gray-600">
-          Name
-        </th>
-        <th className="px-4 py-3 text-left font-medium text-gray-600">
-          Orders
-        </th>
-      </tr>
-    </thead>
+    <div className="max-w-6xl mx-auto mt-6 bg-white border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+      <table className="w-full table-fixed text-sm">
+        <thead className="bg-gray-50 border-b border-b-gray-200">
+          <tr>
+            <th className="px-4 py-3 text-left font-medium text-gray-600">
+              Name
+            </th>
+            <th className="px-4 py-3 text-left font-medium text-gray-600">
+              Orders
+            </th>
+          </tr>
+        </thead>
 
-    <tbody className="divide-y  divide-gray-200  ">
-      <tr className="hover:bg-gray-50 transition">
-        <td className="px-4 py-3 text-gray-700">
-          fii
-        </td>
-        <td className="px-4 py-3 text-gray-700">
-          figidg
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+        <tbody className="divide-y  divide-gray-200  ">
+          <tr className="hover:bg-gray-50 transition">
+            <td className="px-4 py-3 text-gray-700">
+              fii
+            </td>
+            <td className="px-4 py-3 text-gray-700">
+              figidg
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
   )
 }
